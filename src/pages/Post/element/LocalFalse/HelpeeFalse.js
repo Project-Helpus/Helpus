91a8f1ccd7a1ed81627c7bcd2e25574f3ddc9583
Suhyeon <heypitcher@hanmail.net>
2023-01-04 18:30:20 +0900

    Feat:헬피 전국 게시글 가져오기

--- src/pages/Post/element/LocalFalse/HelpeeFalse.js
+++ src/pages/Post/element/LocalFalse/HelpeeFalse.js
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-
+import { __getHelpeeFalse } from "../../../../redux/modules/postSlice";
 const HelpeeFalse = () => {
   const dispatch = useDispatch();
-  // const real = useSelector((state))
+  const real = useSelector((state) => state.postSlice.HelpeeFalseDate)
+  console.log('헬피 false real:', real)
 
 
-
-  // useEffect()
+  useEffect(() => dispatch(__getHelpeeFalse()), [dispatch])
   return (<>
     <StHelpeeWrapper>
       헬피(전국)
