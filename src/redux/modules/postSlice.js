f46f97e18fdca738042ae04a30334dc7deb6a351
Hanul <110656247+hanul-dev@users.noreply.github.com>
2023-01-05 11:13:29 +0900

    Feat: 게시물 생성 기능 추가

--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -1,11 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { PostAPI } from '../../api/axios';
+import { PostAPI } from "../../api/axios";
+import { client } from "../../api/axios";
 
-export const __function = createAsyncThunk(
-  "mypageSlice/",
-  async (data, thunkAPI) => {
+export const __createPost = createAsyncThunk(
+  "mypageSlice/createPost",
+  async (formData, thunkAPI) => {
     try {
-      const response = PostAPI(data);
+      const response = await PostAPI.postCreate(formData);
+      console.log(response);
       return thunkAPI.fulfillWithValue();
     } catch (err) {
       return thunkAPI.rejectWithValue();
@@ -13,13 +15,27 @@ export const __function = createAsyncThunk(
   }
 );
 
-const initialState = {};
+const initialState = {
+  isLoading: false,
+  error: false,
+};
 
 const postSlice = createSlice({
   name: "postSlice",
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: {
+    [__createPost.pending]: state => {
+      state.isLoading = true;
+    },
+    [__createPost.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [__createPost.rejected]: state => {
+      state.isLoading = false;
+      state.error = true;
+    },
+  },
 });
 
 export default postSlice.reducer;
