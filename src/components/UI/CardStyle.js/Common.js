674dae40d5130db0eaeca500f62cf60a36b10274
Suhyeon <heypitcher@hanmail.net>
2023-01-11 16:21:58 +0900

    Feat:카드 컴포넌트 추상화(내 게시물,찜 게시물,메인,케러셀)

---
+++ src/components/UI/CardStyle.js/Common.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const StFlex = styled.div`
+  display: flex;
+`;
+
+export const StCirclePhoto = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin:0 10px 10px 0;
+`;
+export const StMainSquarePhoto = styled.img`
+width:240px;
+height:135px;
+`
+export const StMySquarePhoto = styled.img`
+width:320px;
+height:180px;`
+export const StZZimSquarePhote = styled.img`
+width:272px;
+height:153px;
+`
+
+export const StCategoryName = styled.p`
+  font-size: "";
+  font-weight: "";
+`;
+export const StDate = styled.p`
+font-size: 12px;
+  font-weight: "";
+`;
+export const StContentsTitle = styled.p`
+  font-size: 20px;
+  font-weight: "";
+  margin-top:10px;
+`;
+export const StMainContentsTitle = styled.p`
+font-size:24px;
+`
+export const StNickname = styled.p`
+  font-size: 18px;
+  font-weight: "";
+`;
+export const StAddress = styled.p`
+  font-size: 12px;
+  font-weight: "";
+`;
+export const StContentsInfo = styled.p`
+  font-size: 14px;
+  font-weight: "";
+`;
+
+export const StMainContentsWrapper = styled.div`
+border:1px solid #000;
+padding:14px 0 14px 18px;
+`
