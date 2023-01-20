8752897c994c4226f9f436c142c381d12faef599
Suhyeon <heypitcher@hanmail.net>
2023-01-20 09:51:11 +0900

    Style:상세 페이지 스타일 컴포넌트 분리

--- src/pages/Post/PostDetail.jsx
+++ src/pages/Post/PostDetail.jsx
@@ -13,10 +13,12 @@ import arrow_forward from "../../asset/arrow_forward.svg";
 import { isCompositeComponent } from "react-dom/test-utils";
 import emptyHeart from "../../asset/emptyHeart.svg";
 import fullHeart from "../../asset/fullHeart.svg";
+import { StFlex } from "../../components/UI/CardStyle.js/StCommon";
+import { Avatar, StBackBtn, StBox, StBtnBox, StChatBtn, StContainer, StContent, StDate, StGroupImgs, StInnerColBox, StInnerRowBox, StProfile, StProfileBox, StTitle, StUpdateButton, StUserInfo, StWishBtn, StZZimImg } from "./StPostDetail";
 const PostDetail = () => {
   const zMsg = useSelector((state) => state.postSlice.ZZimMsg.message);
   const userId = useSelector((state) => state.mypageSlice.profile.userId);
-  const deadLine = useSelector((state) => state.postSlice.postInfo.isDeadLine);
+  const deadLine = useSelector((state) => state.postSlice.postInfo.isDeadLine); 
   const navigate = useNavigate();
   const { postId } = useParams();
   const dispatch = useDispatch();
@@ -24,6 +26,7 @@ const PostDetail = () => {
   const { userInfo } = useSelector((state) => state.userSlice);
 
   const { state } = useLocation();
+  console.log('info:',state)
   const deletePost = () => {
     if (userId != state.data.userId) {
       alert("게시물 생성자만 해당 게시글을 삭제할 수 있습니다");
@@ -73,7 +76,25 @@ const PostDetail = () => {
         <StBackBtn onClick={() => navigate(-1)}>
           <img src={arrow_forward} alt="back_button" />
         </StBackBtn>
-        <StTitle>{state.data.title}</StTitle>
+
+        <StUserInfo>
+        <StFlex>
+          <StProfile src={state.data.userImage}></StProfile>
+          <div>
+            <StFlex>
+              <StTitle>{state.data.title}</StTitle>
+              <p>마감</p>
+              </StFlex>
+          <div>{state.data?.userName}</div>
+          <div>{state.data?.location1}</div>
+          </div>
+        </StFlex>
+        <p>하트,2022.12.31</p>
+        </StUserInfo>
+        
+        <StProfileBox>
+          <StInnerRowBox>
+
         {userId !== state.data.userId || (
           <StUpdateButton onClick={updatePost}>수정</StUpdateButton>
         )}
@@ -94,17 +115,6 @@ const PostDetail = () => {
             )}
           </>
         )}
-        <StProfileBox>
-          <StBox>
-            <Avatar>
-              <img src={state.data?.userImage} alt="profile" />
-            </Avatar>
-            <StInnerColBox>
-              <div>{state.data?.userName}</div>
-              <div>{state.data?.location1}</div>
-            </StInnerColBox>
-          </StBox>
-          <StInnerRowBox>
             <span>조회수</span>
             <span>{state.data?.createdAt}</span>
           </StInnerRowBox>
@@ -138,97 +148,3 @@ const PostDetail = () => {
 
 export default PostDetail;
 
-const StContainer = styled.div`
-  width: 86.1%;
-`;
-
-const StBackBtn = styled.button`
-  width: 36px;
-  height: 36px;
-  border: none;
-  background-color: transparent;
-`;
-
-const StTitle = styled.h2`
-  text-align: center;
-`;
-
-const StProfileBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const StBox = styled.div`
-  display: flex;
-`;
-
-const StGroupImgs = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 300px;
-  height: 100px;
-  gap: 10px;
-`;
-const StBtnBox = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 40px;
-`;
-const StInnerColBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const StInnerRowBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-end;
-`;
-
-const Avatar = styled.div`
-  width: 98px;
-  height: 98px;
-  object-fit: cover;
-  border-radius: 100%;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-`;
-
-const StDate = styled.span`
-  padding-top: 27px;
-  font-weight: 700;
-`;
-
-const StContent = styled.div``;
-const StChatBtn = styled.button`
-  width: 200px;
-  height: 44px;
-  background: #ffc4d5;
-  border-radius: 10px;
-  color: white;
-  font-weight: 700;
-  font-size: 18px;
-  border: none;
-`;
-
-const StWishBtn = styled.button`
-  width: 112px;
-  vertical-align: middle;
-  border: none;
-  background: white;
-  font-weight: 700;
-  font-size: 24px;
-`;
-
-const StUpdateButton = styled.button`
-  width: 100px;
-  height: 50px;
-  cursor: pointer;
-`;
-const StZZimImg = styled.img``;
---
+++ src/pages/Post/StPostDetail.js
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+
+
+export const StContainer = styled.div`
+  width: 86.1%;
+`;
+
+export const StBackBtn = styled.button`
+  width: 36px;
+  height: 36px;
+  border: none;
+  background-color: transparent;
+`;
+
+export const StTitle = styled.h2`
+  text-align: center;
+`;
+
+export const StProfileBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const StBox = styled.div`
+  display: flex;
+`;
+
+export const StGroupImgs = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 300px;
+  height: 100px;
+  gap: 10px;
+`;
+export const StBtnBox = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+`;
+export const StInnerColBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const StInnerRowBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+`;
+
+export const Avatar = styled.div`
+  width: 98px;
+  height: 98px;
+  object-fit: cover;
+  border-radius: 100%;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export const StDate = styled.span`
+  padding-top: 27px;
+  font-weight: 700;
+`;
+
+export const StContent = styled.div``;
+export const StChatBtn = styled.button`
+  width: 200px;
+  height: 44px;
+  background: #ffc4d5;
+  border-radius: 10px;
+  color: white;
+  font-weight: 700;
+  font-size: 18px;
+  border: none;
+`;
+
+export const StWishBtn = styled.button`
+  width: 112px;
+  vertical-align: middle;
+  border: none;
+  background: white;
+  font-weight: 700;
+  font-size: 24px;
+`;
+
+export const StUpdateButton = styled.button`
+  width: 100px;
+  height: 50px;
+  cursor: pointer;
+`;
+export const StZZimImg = styled.img``;
+
+export const StProfile = styled.img`
+width:100px;
+height:100px;`
+
+export const StUserInfo = styled.div`
+width:932px;
+height:100px;
+border:1px solid #000;
+`
