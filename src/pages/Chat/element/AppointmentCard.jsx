33158b466064a6e121b801d8a01fd1bdfcb710f3
Hanul <110656247+hanul-dev@users.noreply.github.com>
2023-01-30 19:03:45 +0900

    Fix: console.log 삭제

--- src/pages/Chat/element/AppointmentCard.jsx
+++ src/pages/Chat/element/AppointmentCard.jsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 import invitation from "../../../asset/invitation.svg";
-import StButton from "../../../c                omponents/UI/StButton";
+import StButton from "../../../components/UI/StButton";
 import { useSelector } from "react-redux";
-import { acception } from "../../../utils/socket";
 
 const AppointmentCard = ({ invitation, accepted, acceptRequest, userId }) => {
   const myId = useSelector((state) => state.userSlice.userInfo.userId);
-  console.log(invitation);
+
   return (
     <StCard>
       <StDiv>
