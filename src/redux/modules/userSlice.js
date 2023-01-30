3e3269b26713c4115282854d0b22d91a08220888
hanul-dev <hanul.developer@gmail.com>
2023-01-31 00:45:49 +0900

    Fix: 로그인 후 화면 처리 코드 수정

--- src/redux/modules/userSlice.js
+++ src/redux/modules/userSlice.js
@@ -65,6 +65,7 @@ export const __postLogin = createAsyncThunk(
     try {
       const res = await UserAPI.login(payload);
       if (res.status === 200) {
+        window.alert("로그인 성공");
         return thunkAPI.fulfillWithValue(res.data);
       } else {
         window.alert("가입하신 이메일, 비밀번호와 다릅니다.");
