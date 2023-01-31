f91de7e26b930c3600fa903166f565e516d7733f
hanul-dev <hanul.developer@gmail.com>
2023-01-31 22:22:36 +0900

    Fix: baseURL test서버 지정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: process.env.REACT_APP_SERVER,
+  baseURL: process.env.REACT_APP_SERVER_TEST,
   withCredentials: true,
 });
 
