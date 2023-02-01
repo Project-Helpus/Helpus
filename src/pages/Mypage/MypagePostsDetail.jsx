fb383c0f4540b146a3a61d3e48258990e7823534
Aekyoung_Kwon <116799513+buddesign@users.noreply.github.com>
2023-02-01 16:43:04 +0900

    Feat: 평가하기 기능 제거

--- src/pages/Mypage/MypagePostsDetail.jsx
+++ src/pages/Mypage/MypagePostsDetail.jsx
@@ -4,16 +4,6 @@ import { useNavigate } from "react-router";
 import { __getMyPage, __getMyposts } from "../../redux/modules/mypageSlice";
 import styled from "styled-components";
 import Card from "../../components/Card";
-import lcon_score1 from "../../asset/lcon_score1.svg";
-import lcon_score2 from "../../asset/lcon_score2.svg";
-import lcon_score3 from "../../asset/lcon_score3.svg";
-import lcon_score4 from "../../asset/lcon_score4.svg";
-import lcon_score5 from "../../asset/lcon_score5.svg";
-import lcon_score6 from "../../asset/lcon_score6.svg";
-import lcon_score7 from "../../asset/lcon_score7.svg";
-import lcon_score8 from "../../asset/lcon_score8.svg";
-import lcon_score9 from "../../asset/lcon_score9.svg";
-import lcon_score10 from "../../asset/lcon_score10.svg";
 
 const Mypage = () => {
   const dispatch = useDispatch();
@@ -61,40 +51,6 @@ const Mypage = () => {
         <StProfileImg src={profile?.userImage} alt="" />
         <StName>{userInfo?.userName}</StName>
         <StEmail>{profile?.email}</StEmail>
-        <StheartWrap>
-          {profile?.score === 0 && <span>평가없음</span>}
-          {profile?.score === 1 && (
-            <StheartImg src={lcon_score1} alt=""></StheartImg>
-          )}
-          {profile?.score === 2 && (
-            <StheartImg src={lcon_score2} alt=""></StheartImg>
-          )}
-          {profile?.score === 3 && (
-            <StheartImg src={lcon_score3} alt=""></StheartImg>
-          )}
-          {profile?.score === 4 && (
-            <StheartImg src={lcon_score4} alt=""></StheartImg>
-          )}
-          {profile?.score === 5 && (
-            <StheartImg src={lcon_score5} alt=""></StheartImg>
-          )}
-          {profile?.score === 6 && (
-            <StheartImg src={lcon_score6} alt=""></StheartImg>
-          )}
-          {profile?.score === 7 && (
-            <StheartImg src={lcon_score7} alt=""></StheartImg>
-          )}
-          {profile?.score === 8 && (
-            <StheartImg src={lcon_score8} alt=""></StheartImg>
-          )}
-          {profile?.score === 9 && (
-            <StheartImg src={lcon_score9} alt=""></StheartImg>
-          )}
-          {profile?.score === 10 && (
-            <StheartImg src={lcon_score10} alt=""></StheartImg>
-          )}
-        </StheartWrap>
-        <StHeartText>{profile?.score}/10점</StHeartText>
         <StState>
           {userInfo?.state1} {userInfo?.state2}
         </StState>
@@ -185,23 +141,10 @@ const StName = styled.div`
 `;
 const StEmail = styled.div`
   font-size: 1em;
-  margin-top: 8px;
+  margin-top: 18px;
   color: ${(props) => props.theme.colors.middleGray};
 `;
 const StState = styled.div`
   color: ${(props) => props.theme.colors.middleGray};
-  margin-top: 8px;
-`;
-
-const StheartWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 4px;
-  padding-top: 10px;
-`;
-const StheartImg = styled.img``;
-
-const StHeartText = styled.span`
-  color: ${(props) => props.theme.colors.subPink};
+  margin-top: 10px;
 `;
