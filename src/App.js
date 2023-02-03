aa8189a149f79a053fbebd763e8f68246f5de96a
hanul-dev <hanul.developer@gmail.com>
2023-02-04 01:30:58 +0900

    Feat: Helpus Guide 페이지 이동 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import styled from "styled-components";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import Router from "./shared/Router";
 import ScrollToTop from "./components/ScrollTop";
+import logo from "./asset/logo_32x32.svg";
 
 if (process.env.REACT_APP_NODE_ENV === "production") {
   disableReactDevTools();
@@ -13,8 +15,40 @@ function App() {
       <Router>
         <ScrollToTop />
       </Router>
+      <StGuideBtn
+        onClick={() =>
+          window.open(
+            "https://soroya.notion.site/Helpus-Guide-898d809d9bc64782a12a92de71064df8",
+            "_blank"
+          )
+        }
+      >
+        <img src={logo} alt="helpus_guide_button"></img>
+        &nbsp;&nbsp;Guide
+      </StGuideBtn>
     </div>
   );
 }
 
 export default App;
+
+const StGuideBtn = styled.button`
+  position: fixed;
+  display: flex;
+  align-items: center;
+  bottom: 3.7%;
+  right: 2%;
+  padding: 14px 20px;
+  font-weight: 800;
+  font-size: 20px;
+  border: none;
+  box-shadow: rgb(255 255 255 / 12%) 0px 0px 2px 0px inset,
+    rgb(0 0 0 / 5%) 0px 0px 2px 1px, rgb(0 0 0 / 22%) 0px 4px 20px;
+  background: linear-gradient(
+    to left,
+    rgba(255, 255, 255, 0.9),
+    rgb(255, 255, 255)
+  );
+  backdrop-filter: blur(30px);
+  border-radius: 22px;
+`;
